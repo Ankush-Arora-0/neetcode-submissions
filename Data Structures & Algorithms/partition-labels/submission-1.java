e7ae472e78a066f4a2db2883eee18a0f0a2147da
ANKUSH ARORA <127427325+Ankush-Arora-0@users.noreply.github.com>
2026-08-15 16:59:19 +0530

    Add: partition-labels - submission-1

---
+++ Data Structures & Algorithms/partition-labels/submission-1.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        char arr[] = s.toCharArray();
+        int [] lastIdx = new int[26];
+        for(int i=0;i<arr.length;i++){
+            int idx = (int)(arr[i] -'a');
+            lastIdx[idx] = i;
+        }
+        int i=0,j=0;
+        System.out.println(Arrays.toString(lastIdx));
+        List<Integer> ans=  new ArrayList<>();
+        while(i<arr.length){
+            int id =i;
+            while(id<=j){
+                int idx = (int)(arr[id] -'a');
+                j  = Math.max(lastIdx[idx],j);
+                id++;
+            }
+
+            ans.add(j-i+1);
+            i = id;
+            j = i;
+
+        }
+        return ans;
+    }
+}
